72c205d49b24b73fb99ce2bd6c57fb8703573686
4110064003 <xuanku114nchu@gmail.com>
2024-09-05 17:28:00 +0800

    0905

---
+++ ESP32/code/position_ato5_.py
@@ -0,0 +1,76 @@
+action_groups_ato5 = [
+      [#1[置中]
+          {'id': 1, 'position': 300, 'time': 1000},
+          {'id': 2, 'position': 500, 'time': 1000},
+          {'id': 3, 'position': 300, 'time': 1000},
+          {'id': 4, 'position': 900, 'time': 1000},
+          {'id': 5, 'position': 700, 'time': 1000},
+          {'id': 6, 'position': 500, 'time': 1000}
+      ],
+      [#2[a上張]
+          {'id': 1, 'position': 300, 'time': 1000},
+          {'id': 2, 'position': 500, 'time': 1000},
+          {'id': 3, 'position': 150, 'time': 1000},
+          {'id': 4, 'position': 800, 'time': 1000},
+          {'id': 5, 'position': 450, 'time': 1000},
+          {'id': 6, 'position': 800, 'time': 1000}
+      ],
+      [#3[a上抓]
+          {'id': 1, 'position': 600, 'time': 1000},
+          {'id': 2, 'position': 500, 'time': 1000},
+          {'id': 3, 'position': 150, 'time': 1000},
+          {'id': 4, 'position': 800, 'time': 1000},
+          {'id': 5, 'position': 450, 'time': 1000},
+          {'id': 6, 'position': 800, 'time': 1000}
+      ],
+      [#4[置中]
+          {'id': 1, 'position': 600, 'time': 1000},
+          {'id': 2, 'position': 500, 'time': 1000},
+          {'id': 3, 'position': 300, 'time': 1000},
+          {'id': 4, 'position': 900, 'time': 1000},
+          {'id': 5, 'position': 700, 'time': 1000},
+          {'id': 6, 'position': 500, 'time': 1000}
+      ],
+      [#5[5上抓]
+          {'id': 1, 'position': 600, 'time': 1000},
+          {'id': 2, 'position': 500, 'time': 1000},
+          {'id': 3, 'position': 150, 'time': 1000},
+          {'id': 4, 'position': 800, 'time': 1000},
+          {'id': 5, 'position': 450, 'time': 1000},
+          {'id': 6, 'position': 500, 'time': 1000}
+      ],
+      [#6[5上放]
+          {'id': 1, 'position': 300, 'time': 1000},
+          {'id': 2, 'position': 500, 'time': 1000},
+          {'id': 3, 'position': 150, 'time': 1000},
+          {'id': 4, 'position': 800, 'time': 1000},
+          {'id': 5, 'position': 450, 'time': 1000},
+          {'id': 6, 'position': 500, 'time': 1000}
+      ],
+      [#7[置中]
+          {'id': 1, 'position': 300, 'time': 1000},
+          {'id': 2, 'position': 500, 'time': 1000},
+          {'id': 3, 'position': 300, 'time': 1000},
+          {'id': 4, 'position': 900, 'time': 1000},
+          {'id': 5, 'position': 700, 'time': 1000},
+          {'id': 6, 'position': 500, 'time': 1000}
+      ]
+  ]
+# 反轉動作組順序，從1點移動回a點
+action_groups_5toa = list(reversed(action_groups_ato5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
